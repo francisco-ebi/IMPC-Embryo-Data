31842fe8cbe00ceedf139370b9adb020340aff63
Francisco Peña <francisco@ebi.ac.uk>
2023-04-30 11:30:27 -0600

    WIP: display heatmap with transformed data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { HeatMapCanvas } from '@nivo/heatmap';
 import './App.css';
+import dataService from '../services/data-service';
+import { ChartData } from '../models/Charts';
 
 function App() {
+  const [chartData, setChartData] = useState<ChartData>([]);
+  useEffect(() => {
+    setChartData(dataService.getDataForChart());
+  }, []);
+
+  console.log(chartData);
   return (
-    <div className="App">
+    <>
       <h1>IMPC Embryo data</h1>
-    </div>
+      <HeatMapCanvas
+        width={1400}
+        height={22000}
+        data={chartData}
+        margin={{ top: 150, right: 60, bottom: 20, left: 80 }}
+        xInnerPadding={0.15}
+        yInnerPadding={0.15}
+        emptyColor='#EEE'
+        colors={{
+          type: 'sequential',
+          scheme: 'blues',
+        }}
+        enableLabels={false}
+        axisTop={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: -45,
+          legend: '',
+          legendOffset: 46
+        }}
+        axisRight={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'gene',
+          legendPosition: 'middle',
+          legendOffset: 40
+        }}
+        isInteractive={false}
+      />
+    </>
   );
 }
 
