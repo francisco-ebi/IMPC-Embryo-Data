da5897faec64b70548a75d1ecd086d33c3a8659b
Francisco Peña <francisco@ebi.ac.uk>
2023-05-04 11:34:41 -0600

    feat: updated Filters component, add checkboxes to enable individual filters

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -4,6 +4,8 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
+import Checkbox from '@mui/material/Checkbox';
+import Stack from '@mui/material/Stack';
 
 import useDebounce from '../../utils/useDebounce';
 
@@ -15,6 +17,8 @@ interface FilterProps {
   onChangeAssociation: (newValue: number) => void;
 }
 
+type CheckboxSelection = 'genes' | 'term' | 'count';
+
 const Filters = ({
   geneNames,
   topLevelTerms,
@@ -25,53 +29,83 @@ const Filters = ({
 
   const [rangeValue, setRangeValue] = useState<number>(90);
   const debouncedRangeValue = useDebounce<number>(rangeValue, 500);
+  const [selectedCheckbox, setSelectedCheckbox] = useState<CheckboxSelection>('genes');
 
-  useEffect(() => onChangeAssociation(debouncedRangeValue), [debouncedRangeValue])
+  useEffect(() => onChangeAssociation(debouncedRangeValue), [debouncedRangeValue]);
+
+  const updateSelection = (newSelection: CheckboxSelection) => {
+    setSelectedCheckbox(newSelection);
+  }
 
   return (
     <>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Autocomplete
-            multiple
-            options={geneNames}
-            onChange={(_, value) => onSelectGene(value)}
-            renderInput={params => (
-              <TextField
-                {...params}
-                variant='standard'
-                label='Filter by gene name'
-                placeholder='Choose a gene...'
-              />
-            )}
-          />
+          <Stack direction="row" spacing={1}>
+            <Checkbox
+              checked={selectedCheckbox === 'genes'}
+              onChange={() => updateSelection('genes')}
+            />
+            <Autocomplete
+              className="input-autocomplete"
+              disabled={selectedCheckbox !== 'genes'}
+              multiple
+              options={geneNames}
+              onChange={(_, value) => onSelectGene(value)}
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  variant='standard'
+                  label='Filter by gene name'
+                  placeholder='Choose a gene...'
+                />
+              )}
+            />
+          </Stack>
         </Grid>
         <Grid item xs={6}>
-          <Autocomplete
-            multiple
-            options={topLevelTerms}
-            onChange={(_, value) => onSelectTerm(value)}
-            renderInput={params => (
-              <TextField
-                {...params}
-                variant='standard'
-                label='Filter by significant phenotype system'
-                placeholder='Choose a phenotype system...'
-              />
-            )}
-          />
+          <Stack direction="row" spacing={1}>
+            <Checkbox
+              checked={selectedCheckbox === 'term'}
+              onChange={() => updateSelection('term')}
+            />
+            <Autocomplete
+              className="input-autocomplete"
+              disabled={selectedCheckbox !== 'term'}
+              multiple
+              options={topLevelTerms}
+              onChange={(_, value) => onSelectTerm(value)}
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  variant='standard'
+                  label='Filter by significant phenotype system'
+                  placeholder='Choose a phenotype system...'
+                />
+              )}
+            />
+          </Stack>
         </Grid>
         <Grid item xs={6}>
-          <Typography gutterBottom>
-            Filter top {rangeValue}% of the genes that have the highest phenotype count
-          </Typography>
-          <Slider
-            max={100}
-            min={10}
-            step={5}
-            value={rangeValue}
-            onChange={(_, value) => setRangeValue(value as number)}
-          />
+          <Stack direction="row" spacing={1}>
+            <Checkbox
+              checked={selectedCheckbox === 'count'}
+              onChange={() => updateSelection('count')}
+            />
+            <div>
+              <Typography gutterBottom>
+                Filter top {rangeValue}% of the genes that have the highest phenotype count
+              </Typography>
+              <Slider
+                disabled={selectedCheckbox !== 'count'}
+                max={100}
+                min={10}
+                step={5}
+                value={rangeValue}
+                onChange={(_, value) => setRangeValue(value as number)}
+              />
+            </div>
+          </Stack>
         </Grid>
       </Grid>
     </>
